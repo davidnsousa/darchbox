35860b9656c157553eab6a7566285111e19c203f
David Sousa <davidnsousa@protonmail.com>
2023-05-07 00:14:43 +0100

    update wifi-menu

--- filesystem/home/user/.config/scripts/wifi_menu.sh
+++ filesystem/home/user/.config/scripts/wifi_menu.sh
@@ -3,5 +3,10 @@
 selected=$(nmcli -t -f ssid dev wifi | grep -E -v '^$' | dmenu -nb '#383c4a' -nf '#ffffff' -sb '#5294e2' -fn 'DejaVu Sans:size=9.6' -p "Connect to Wi-FI:")
 
 if [[ -n "$selected" ]]; then
-  $TERMINAL -e nmcli device wifi connect "$selected"
+	if nmcli -s -g 802-11-wireless-security.psk connection show '$selected' 2>&1 | grep -q "no such connection profile"; then
+		$TERMINAL -e nmcli --ask device wifi connect "$selected"
+    else
+		nmcli device wifi connect "$selected"
+	fi
 fi
+
