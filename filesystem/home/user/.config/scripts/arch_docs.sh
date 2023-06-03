7d3ab0da0ebcee96bfdf6bb9c0a7286bffd0778c
David Sousa <davidnsousa@protonmail.com>
2023-06-04 00:43:26 +0100

    update arch_docs.sh

--- filesystem/home/user/.config/scripts/arch_docs.sh
+++ filesystem/home/user/.config/scripts/arch_docs.sh
@@ -2,4 +2,8 @@
 
 search_term=$(curl -sS "https://wiki.archlinux.org/title/Table_of_contents" | grep -o '<a href="/title/Category:[^>]*>[^<]*</a>' | sed 's/<[^>]*>//g' | eval "dmenu $DMENU_ARGS -p 'Search Arch docs:'")
 
-surf "https://wiki.archlinux.org/index.php?search=${search_term}"
+if [ -n "$search_term" ]; then
+    surf "https://wiki.archlinux.org/index.php?search=${search_term}"
+fi
+
+
