281ac21d54f51ecb6d81327f410f3b11ffab80a9
davidnsousa <davidnsousa@pm.me>
2025-04-01 06:15:23 +0100

    update polybar battery

--- filesystem/home/user/.config/polybar/scripts/battery.sh
+++ filesystem/home/user/.config/polybar/scripts/battery.sh
@@ -2,20 +2,27 @@
 
 status () {
         BATDEVICE=$(echo /sys/class/power_supply/$(ls /sys/class/power_supply/ | grep BAT))
-        capacity=$(cat $BATDEVICE/capacity)
-        status=$(cat $BATDEVICE/status)
-        if [ $status = 'Charging' ]; then
-                echo "%{F#FFFF00}↑%{F-} $capacity%"
-        elif [ $status = 'Full' ]; then
-                echo "%{F#FFFF00}-%{F-}"
-        elif [ $status = 'Discharging'  ] || [ $status = 'Not Charging' ]; then
-                if [ $capacity -le 10 ]; then
-                        echo "%{F#FF0000}↓ $capacity%%{F-}"
-                elif [ $capacity -le 20 ]; then
-                        echo "%{F#FFA500}↓ $capacity%%{F-}"
-                else
-                        echo "↓ $capacity%"
-                fi
+        capacity=$(cat "$BATDEVICE/capacity")
+        status=$(cat "$BATDEVICE/status")
+        if [ -z "$capacity" ] || [ -z "$status" ]; then
+            echo "Error: Unable to retrieve battery information."
+            exit 1
+        fi
+
+        if [ "$status" = "Charging" ]; then
+            echo "%{F#FFFF00}↑%{F-} $capacity%"
+        elif [ "$status" = "Not charging" ]; then
+            echo "%{F#FFFF00}-%{F-}"
+        elif [ "$status" = "Discharging" ] || [ "$status" = "Not Charging" ]; then
+            if [ "$capacity" -le 10 ]; then
+                echo "%{F#FF0000}↓ $capacity%%{F-}"
+            elif [ "$capacity" -le 20 ]; then
+                echo "%{F#FFA500}↓ $capacity%%{F-}"
+            else
+                echo "↓ $capacity%"
+            fi
+        else
+            echo "Unknown status: $status"
         fi
 }
 
